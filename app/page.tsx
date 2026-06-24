cd4f1ce984440321a2a45bdf579cb3b31154d589
Mizan <mail.kazimizan@gmail.com>
2026-06-24 10:57:47 +0600

    Remove visa services component from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-
-
-
 import Carousel from "./components/Carousel";
 import PromotionalBanner from "./components/PromotionalBanner";
 import ServicesSection from "./components/Services";
 import TopContries from "./components/TopContries";
-
-import VisaServices from "./components/VisaServices";
 import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Home() {
@@ -15,7 +10,6 @@ export default function Home() {
       <Carousel/>
       <ServicesSection />
       <TopContries />
-      <VisaServices />
       <WhyChooseUs />
       <PromotionalBanner />
     </>
